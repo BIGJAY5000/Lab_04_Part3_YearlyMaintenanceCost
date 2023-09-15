e52e023d92cb52ec2159f1aa6edae0d6dc700e0f
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-09-15 09:19:02 -0400

    maintenance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+
+    //variable declarations
+    double winter;
+    double summer;
+    double spring;
+    double fall;
+    double yearly;
+
+    System.out.println("Please enter the maintenance cost of the winter season");
+    winter = scan.nextDouble();
+    System.out.println("Please enter the maintenance cost of the spring season");
+    spring = scan.nextDouble();
+    System.out.println("Please enter the maintenance cost of the summer season");
+    summer = scan.nextDouble();
+    System.out.println("Please enter the maintenance cost of the fall season");
+    fall = scan.nextDouble();
+    yearly = winter + summer + spring + fall;
+    System.out.println("The total yearly maintenance cost is $" + yearly + ".");
     }
+
 }
